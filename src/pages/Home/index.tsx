cee75d4cd67c26f8b298f9def91bd6bfe42907ef
leonace924 <meobius123@gmail.com>
2022-04-27 15:31:13 -0400

    feat: add routes and basic 404 and home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Title } from 'components/Common';
+
+const HomePage = () => {
+  return (
+    <View>
+      <Title className="text-3xl text-blue-600">Home Page</Title>
+    </View>
+  );
+};
+
+export default HomePage;
