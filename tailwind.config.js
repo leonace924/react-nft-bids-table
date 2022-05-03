06dd8f44cbd46fca2ae6cfe8bc13c61edaf0aac0
leonace924 <meobius123@gmail.com>
2022-05-03 06:41:12 -0400

    fix: clean tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    backgroundColor: (theme) => ({
-      ...theme('colors'),
-    }),
-    borderColor: (theme) => ({
-      ...theme('colors'),
-    }),
-    extend: {
-      colors: {},
-    },
-  },
+  theme: {},
   plugins: [],
 };
