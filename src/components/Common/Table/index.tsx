6968eb9bde651dd8ce288e73889378af891006d2
leonace924 <meobius123@gmail.com>
2022-05-03 06:40:07 -0400

    fix: update loading and table component

--- src/components/Common/Table/index.tsx
+++ src/components/Common/Table/index.tsx
@@ -3,6 +3,8 @@ import cx from 'classnames';
 import { Error, Pagination, View } from 'components/Common';
 import { useCurrencyFormatter } from 'hooks/useCurrencyFormatter';
 import { TableColumnList } from 'lib/constant';
+import { useExpiredBid } from 'hooks/useExpiredBid';
+import { useRelativeTime } from 'hooks/useRelativeTime';
 
 type TableProps<T> = {
   data?: T[];
@@ -23,6 +25,8 @@ const TableCell = ({ as = 'th', className, children }: TableCellProps) => {
 export const Table = <T extends object>({ data, pageSize = 10 }: TableProps<T>) => {
   const [currentPage, setCurrentPage] = useState(1);
   const formatCurrency = useCurrencyFormatter();
+  const relativeTime = useRelativeTime();
+  const isExpiredBid = useExpiredBid();
 
   if (!data || !data.length) return <Error type="noData" />;
 
@@ -34,7 +38,7 @@ export const Table = <T extends object>({ data, pageSize = 10 }: TableProps<T>) 
 
   return (
     <View className="inline-block">
-      <table className="border-separate border border-slate-400">
+      <table className="border border-separate border-slate-400">
         <thead className="bg-slate-50">
           <tr>
             {TableColumnList.map((column) => (
@@ -44,18 +48,18 @@ export const Table = <T extends object>({ data, pageSize = 10 }: TableProps<T>) 
         </thead>
         <tbody>
           {currentTableData?.map((item: any) => (
-            <tr key={item.id}>
+            <tr key={item.id} className={cx({ 'bg-rose-200': isExpiredBid(item.expiration) })}>
               <TableCell as="td">{item.id}</TableCell>
               <TableCell as="td">{formatCurrency(item.price)}</TableCell>
               <TableCell as="td">{item.bidder}</TableCell>
-              <TableCell as="td">{item.expiration}</TableCell>
+              <TableCell as="td">{relativeTime(item.expiration)}</TableCell>
             </tr>
           ))}
         </tbody>
       </table>
       <View className="text-center">
         <Pagination
-          className="mt-4 inline-flex items-center gap-4"
+          className="inline-flex items-center gap-4 mt-4"
           currentPage={currentPage}
           totalCount={data.length}
           pageSize={pageSize}
